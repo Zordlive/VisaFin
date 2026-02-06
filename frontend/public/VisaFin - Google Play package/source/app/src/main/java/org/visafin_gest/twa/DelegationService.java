ea832b254804574e98b918b4e9f6695135279ee9
Zordlive <martin.lub999@gmail.com>
2026-02-06 15:30:21 +0100

    Update

---
+++ frontend/public/VisaFin - Google Play package/source/app/src/main/java/org/visafin_gest/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package org.visafin_gest.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
